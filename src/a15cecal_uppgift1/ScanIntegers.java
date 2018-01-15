47f2d9e8182265152d265f20fcc1c4465a20d063
Zander <31848787+Zanalm@users.noreply.github.com>
2018-01-15 15:32:17 +0100

    Minor corrections
    some added comments and edited output message

--- src/a15cecal_uppgift1/ScanIntegers.java
+++ src/a15cecal_uppgift1/ScanIntegers.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-//scans text and parse it
 public class ScanIntegers {
 
+// scan is a function that scans text and parses it
+// Scanner is declared as a local variable 
+// Scanner is then closed in the end of the function
 // Asks the user to decide how many numbers to input than puts the value in an array
 public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
@@ -18,9 +20,10 @@ public static void main(String[] args) {
 
 }
 
+// Scanner2 is declared, than closed as for the first one
 // Tells the number the user wrote in earlier, and asks for the numbers to be sorted
 public static int[] getIntegers(int number) {
-    System.out.println("Enter " + number + " numbers.");
+    System.out.println("Enter " + number + " numbers with a space between them.");
     int[] values = new int[number];
 	Scanner scanner2 = new Scanner(System.in);
     for (int i = 0; i < values.length; i++) {
@@ -31,6 +34,7 @@ public static int[] getIntegers(int number) {
 }
 
 public static int[] sortIntegers(int[] unsorted) {
+	// Copies the specified array with the copyOf and truncates it
     int[] numbers = Arrays.copyOf(unsorted, unsorted.length);
 
     // Pushes the smallest number down
